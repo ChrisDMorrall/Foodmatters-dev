d36bede3e14c09ea07657b6fac957da3f34950ff
Chris Morrall <chris@chrismorrall.me>
2018-10-01 17:36:43 +0200

    First Commit

---
+++ dist/index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<!-- Start your project here-->
+<div style="height: 100vh">
+  <div class="flex-center flex-column">
+    <h1 class="animated fadeIn mb-4">Wordpress 4.9.8 Theme with MDB 4.5.11 integration</h1>
+
+    <h5 class="animated fadeIn mb-3">Have fun and code safely!</h5>
+
+    <p class="animated fadeIn text-muted">By Chris Morrall</p>
+  </div>
+</div>
+<!-- /Start your project here-->
+<?php get_footer(); ?>
